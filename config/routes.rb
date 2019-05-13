e4da41f6b34fb97e9ab40bcf4c577c9d7b17c0f7
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-05-13 13:24:31 -0400

    route changes to items

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'items/create'
-  end
-  namespace :api do
-    get 'lists/create'
-  end
-  namespace :api do
-    get 'users/index'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :users do
@@ -15,9 +6,9 @@ Rails.application.routes.draw do
     end
 
     resources :lists, only: [] do
-      resources :items, only: [:create]
+      resources :items, only: [:index, :create, :update, :destroy]
     end
 
-    resources :items, only: [:destroy]
+    resources :items, only: [:index, :create, :update, :destroy]
   end
 end
